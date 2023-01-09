f6c34929b134db626e0075e9658e38e232acf187
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-08 21:38:57 -0500

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order.
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending.
  * @list: Is the pointer to the first node.
 */
 
